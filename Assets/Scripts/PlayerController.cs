409eccbe991a17452d4bdbe403973b57eee5378a
Tom Sell <thomas.j.sell@gmail.com>
2015-02-03 14:58:41 -0500

    added walls, collectables and scoring.  version 1.0 complete

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,16 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
   public float speed;
+  public GUIText countText;
+  public GUIText winText;
+  private int count;
+
+  void Start()
+  {
+    count = 0;
+    SetCountText();
+    winText.text = "";
+  }
 
   void FixedUpdate ()
   {
@@ -13,5 +23,24 @@ public class PlayerController : MonoBehaviour {
     Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
     rigidbody.AddForce(movement * speed * Time.deltaTime);
+  }
+
+  void OnTriggerEnter(Collider other)
+  {
+    if(other.gameObject.tag == "PickUp")
+    {
+      other.gameObject.SetActive(false);
+      count = count + 1;
+      SetCountText();
+    }
+  }
+
+  void SetCountText ()
+  {
+    countText.text = "count: " + count.ToString();
+    if(count >= 10)
+    {
+      winText.text = "YOU WIN!";
+    }
   }
 }
